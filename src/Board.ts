ee15c404e2b2caa17dade5881696f83c784aa0cc
Daniel Ramos <danielramosacosta1@gmail.com>
2024-05-15 21:57:24 +0200

    refactor

--- src/Board.ts
+++ src/Board.ts
@@ -11,36 +11,13 @@ export class Coordinates {
   getI() {
     return this.i
   }
+
   getJ() {
     return this.j
   }
-}
 
-export class Board {
-  private readonly cells: boolean[][]
-  constructor(cells: Array<Array<boolean>>) {
-    this.cells = cells
-  }
-  toString() {
-    return this.cells
-      .map((row) => row.map((cell) => (cell ? "+" : " ")))
-      .map((row) => row.join(""))
-      .join("\n")
-  }
-  nextGeneration(): Board {
-    const newCells = this.cells.map((row, i) => row.map((cell, j) => this.getNextGeneration(i, j, cell)))
-    return new Board(newCells)
-  }
-
-  getNextGeneration(i: number, j: number, cell: boolean) {
-    const neighbors = this.getNeighbors(Coordinates.at(i, j))
-
-    const aliveNeighbors = neighbors.filter(Boolean).length === 2
-    return aliveNeighbors && cell
-  }
-
-  getNeighbors(coordinates: Coordinates) {
-    const neighbors = []
+  getNeighbors() {
+    const coordinates = this
     const neighborsCoordinates: Coordinates[] = []
 
     for (let i = coordinates.getI() - 1; i <= coordinates.getI() + 1; i++) {
@@ -52,12 +29,45 @@ export class Board {
       }
     }
 
-    for (const neighborsCoordinate of neighborsCoordinates) {
-      const rowOfCells = this.cells[neighborsCoordinate.getI()] ?? []
-      const cell = rowOfCells[neighborsCoordinate.getJ()] ?? false
-      neighbors.push(cell)
+    return neighborsCoordinates
+  }
+}
+
+export class Board {
+  private readonly cells: boolean[][]
+  constructor(cells: Array<Array<boolean>>) {
+    this.cells = cells
+  }
+
+  nextGeneration(): Board {
+    const newCells = this.cells.map((row, i) => row.map((cell, j) => this.getNextGeneration(i, j, cell)))
+    return new Board(newCells)
+  }
+
+  getNextGeneration(i: number, j: number, cell: boolean) {
+    const neighbors = this.getNeighbors(Coordinates.at(i, j))
+    const hasTwoAliveNeighbors = neighbors.filter(Boolean).length === 2
+    return hasTwoAliveNeighbors && cell
+  }
+
+  getNeighbors(coordinates: Coordinates) {
+    return coordinates.getNeighbors().map((coordinate) => this.getCellAt(coordinate))
+  }
+
+  private getCellAt(neighborsCoordinate: Coordinates) {
+    const row = this.cells[neighborsCoordinate.getI()]
+
+    if (!row) {
+      return false
     }
 
-    return neighbors
+    return row[neighborsCoordinate.getJ()] ?? false
+  }
+
+  toString() {
+    return this.cells
+      .map((row) => row.map((cell) => (cell ? "+" : " ")))
+      .map((row) => row.join(""))
+      .join("\n")
   }
 }
